5ec1d038e449c238ec47f36f92dd2a4099971a4c
skz0407 <sugakko0507@gmail.com>
2024-10-29 15:23:07 +0900

    10/29-04

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -7,7 +7,7 @@ export default function Hello() {
   useEffect(() => {
     async function getMessage() {
       try {
-        const url = "https://vercel.com/new/skz0407s-projects/success?developer-id=&external-id=&redirect-url=&branch=main&deploymentUrl=fastapi-dds7p5umf-skz0407s-projects.vercel.app&projectName=fastapi&s=https%3A%2F%2Fgithub.com%2Fskz0407%2Ffastapi&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1";
+        const url = "https://fastapi-lake.vercel.app/?vercelToolbarCode=cr0gMap313B0EFp";
         const res = await axios.get(url);
         setMessage(res.data.message);
       } catch (err) {
